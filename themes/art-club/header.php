6eac37416a962fd9e9f1dcca983f6d8ab21726b9
Matthew Hernandez <matt.hernandez94@gmail.com>
2021-01-05 16:06:54 -0500

    wrote header markup

--- themes/art-club/header.php
+++ themes/art-club/header.php
@@ -6,4 +6,13 @@
   </head>
 
   <body>
-    <h1>art club</h1>
+    <header class="wrapper">
+      <h1 class="header__title">Art Club</h1>
+      <nav class="header__nav">
+        <ul class="nav__list">
+          <li class="nav__item"><a href="#" class="nav__link">Places</a></li>
+          <li class="nav__item"><a href="#" class="nav__link">Arists</a></li>
+          <li class="nav__item"><a href="#" class="nav__link">Artwork</a></li>
+        </ul>
+      </nav>
+    </header>
