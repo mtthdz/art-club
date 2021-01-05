630fabc358d8b213c30892b97ed08aa58d9eb918
Matthew Hernandez <matt.hernandez94@gmail.com>
2021-01-05 16:44:57 -0500

    added proper meta data to head

--- themes/art-club/header.php
+++ themes/art-club/header.php
@@ -1,18 +1,20 @@
 <!DOCTYPE html>
-<html lang="en">
-  <head>
-    <?php wp_head(); ?>
-    <title>Art Club</title>
-  </head>
+<html <?php language_attributes(); ?>>
+<head>
+  <meta charset="<?php bloginfo('charset'); ?>">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <?php wp_head(); ?>
+  <title>Art Club</title>
+</head>
 
-  <body>
-    <header class="wrapper">
-      <h1 class="header__title">Art Club</h1>
-      <nav class="header__nav">
-        <ul class="nav__list">
-          <li class="nav__item"><a href="#" class="nav__link">Places</a></li>
-          <li class="nav__item"><a href="#" class="nav__link">Arists</a></li>
-          <li class="nav__item"><a href="#" class="nav__link">Artwork</a></li>
-        </ul>
-      </nav>
-    </header>
+<body>
+  <header class="wrapper">
+    <h1 class="header__title">Art Club</h1>
+    <nav class="header__nav">
+      <ul class="nav__list">
+        <li class="nav__item"><a href="#" class="nav__link">Places</a></li>
+        <li class="nav__item"><a href="#" class="nav__link">Arists</a></li>
+        <li class="nav__item"><a href="#" class="nav__link">Artwork</a></li>
+      </ul>
+    </nav>
+  </header>
