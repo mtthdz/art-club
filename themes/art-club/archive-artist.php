7cdb04c7e31fc803959229e76609d88bbaf57e39
Matthew Hernandez <matt.hernandez94@gmail.com>
2021-01-07 16:16:08 -0500

    added refactor note to archive pages

--- themes/art-club/archive-artist.php
+++ themes/art-club/archive-artist.php
@@ -1,4 +1,12 @@
-<?php get_header(); ?>
+<?php get_header(); 
+// TODO:
+// we should consider making the places and artists post types more dynamic
+// rather than using archive pages to maintain unique layouts between the two
+// potential idea:
+// create a conditional on if the_post() is place or artist post types
+// the result would pull in whatever template part from the templates folder 
+// this will keep the post type layouts unique without requiring two different archives
+?>
 
 <section class="artist__gallery">
 
