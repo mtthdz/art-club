6cc66bba630ec44fe9bb1d498eb414f39be3822d
Matthew Hernandez <matt.hernandez94@gmail.com>
2021-01-10 18:11:45 -0500

    added google fonts

--- themes/art-club/functions.php
+++ themes/art-club/functions.php
@@ -3,6 +3,7 @@
 // css & js import
 function artFiles() {
   wp_enqueue_style('main_styles', get_stylesheet_uri());
+  wp_enqueue_style('custom-google-fonts', '//fonts.googleapis.com/css2?family=Playfair+Display:wght@400;700&display=swap');
 }
 
 function themeFeatures() {
