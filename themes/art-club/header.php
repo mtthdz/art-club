d435ba88be6041c508482a26bebeeba6e2b12250
Matthew Hernandez <matt.hernandez94@gmail.com>
2021-01-12 11:55:25 -0500

    starting nav conditional styling

--- themes/art-club/header.php
+++ themes/art-club/header.php
@@ -13,13 +13,20 @@
       <a href="<?php echo site_url() ?>" class="home__link">ac.</a>
     </h1>
 
+    <!-- TODO
+      conditional styling isn't working with navigation elements
+    -->
     <nav class="header__nav">
       <ul class="nav__list">
         <li class="nav__item">
           <a href="<?php echo get_post_type_archive_link('place') ?>" class="nav__link">Places</a>
         </li>
         <li class="nav__item">
-          <a href="<?php echo get_post_type_archive_link('artist') ?>" class="nav__link">Arists</a>
+          <a           
+            href="<?php echo get_post_type_archive_link('artist') ?>" 
+            class="nav__link"
+            <?php if(get_post_type() == 'artist' or is_page('artists')) echo 'class="nav__link--current"' ?>
+          >Arists</a>
         </li>
         <li class="nav__item">
           <a href="<?php echo site_url() ?>" class="nav__link">About</a>
